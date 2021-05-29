bd728f2eef0079be0445b93e8af2cfc8859d3adf
TatiPav <pttist2012@gmail.com>
2021-05-30 01:12:00 +0300

    arrows2

--- examples/demo.py
+++ examples/demo.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-X = np.arange(-100, 500, 30)
-Y = np.arange(-100, 100, 20)
+X = np.arange(-400, 400, 5)
+Y = np.arange(-400, 400, 5)
 U, V = np.meshgrid(X, Y)
 
 fig, ax = plt.subplots()
-ax.scatter(U,V, c='orange')
+ax.scatter(U,V, c='blue')
 q = ax.quiver(X, Y, U, V)
 ax.quiverkey(q, X=5.5, Y=1.1, U=100,
              label='Quiver key, length = 10', labelpos='E')
@@ -15,4 +15,4 @@ ax.quiverkey(q, X=5.5, Y=1.1, U=100,
 # plt.show()
 
 # 2 Сохранение диаграммы в файле с отсечением лишнего пространства
-plt.savefig('demo1.png', bbox_inches='tight')
+plt.savefig('demo2.png', bbox_inches='tight')
