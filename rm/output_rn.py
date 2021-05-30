3fb64d1548be6529cb73416d225499b9c7bd28c5
TatiPav <pttist2012@gmail.com>
2021-05-30 18:06:45 +0300

    dark

---
+++ rm/output_rn.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+from r_numbers import  RNumbers
+
+# Строим случайное блуждение
+while True:
+    rn = RNumbers()
+    rn.fill_numbers()
+
+    # Наносим точки на диаграмму
+    plt.style.use('dark_background')
+    fig, ax = plt.subplots()
+
+    keep_running = input("Для выполнения нажмите 'y', а потом 'n' для записи в файл): ")
+    if keep_running == 'n':
+        break
+
+ax.scatter(rn.x_values, rn.y_values, s=15)
+
+# 1 Вывод диаграммы
+# plt.show()
+
+# 2 Сохранение диаграммы в файле с отсечением лишнего пространства
+plt.savefig('output.png', bbox_inches='tight')
---
+++ rm/r_numbers.py
@@ -0,0 +1,33 @@
+from random import choice
+
+class RNumbers:
+    def __init__(self, n_points=5000):
+        self.n_points = n_points
+
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_numbers(self):
+        while len(self.y_values) < self.n_points:
+            # Перемещаемся в направлении до достижения предельной длины, вправо/влево
+            x_direction = choice([1, -1])
+            # расстояние/выбор  дальности перемещения
+            x_distance = choice([0, 1, 2, 3, 4])
+            # шаг/направление/расстояние
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            # если оба равны нулю
+            if x_step == 0 and y_step == 0:
+                continue
+
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
+
+
