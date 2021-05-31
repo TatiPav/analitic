0fe9ca9b0050ae7088a88944c9b42664219e9ea8
TatiPav <pttist2012@gmail.com>
2021-05-31 23:14:48 +0300

    old

--- examples/demo.py
+++ examples/demo.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from plotly import offline
 
 X = np.arange(-400, 400, 5)
 Y = np.arange(-400, 400, 5)
@@ -12,7 +13,7 @@ ax.quiverkey(q, X=5.5, Y=1.1, U=100,
              label='Quiver key, length = 10', labelpos='E')
 
 # 1 Вывод диаграммы
-# plt.show()
+plt.show()
 
 # 2 Сохранение диаграммы в файле с отсечением лишнего пространства
-plt.savefig('demo2.png', bbox_inches='tight')
+# plt.savefig('demo2.png', bbox_inches='tight')
