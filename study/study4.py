8e7f520a02719e274f869a953320906402ba3b80
TatiPav <pttist2012@gmail.com>
2021-06-04 23:01:41 +0300

    map1

--- study/study4.py
+++ study/study4.py
@@ -3,32 +3,36 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Изучение структуры представленных данных
-filename = 'data/day_m1.json'
+filename = 'data/day30_m1.json'
 with open(filename) as f:
     # Преобразование в удобный формат (словарь)
     all_data = json.load(f)
 all_dicts = all_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for e_dict in all_dicts:
     mag = e_dict['properties']['mag']
     lon = e_dict['geometry']['coordinates'][0]
     lat = e_dict['geometry']['coordinates'][1]
+    title = e_dict['properties']['title']
 
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
-# print(mags[:10])
-# print(lons[:5])
-# print(lats[:5])
 # Наносим данные на карту
 data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
         'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Picnic',
+        'reversescale': True,
+        'colorbar': {'title': 'Величина'},
     },
 }]
 
