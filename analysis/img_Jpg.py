7f28341f3a543938576e330823d2c4313b7ca2ed
TatiPav <pttist2012@gmail.com>
2021-06-13 18:04:44 +0300

    chess

--- analysis/img_Jpg.py
+++ analysis/img_Jpg.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
-html = urlopen('https://alexbrush.com/nastrojka-postoyannyh-ssylok-s-html-na-kontse.html')
+html = urlopen('https://chesswood.ru/biography/chempiony-mira-po-shahmatam.html')
 one = BeautifulSoup(html, 'html.parser')
 
 images = one.find_all('img', {'src':re.compile('\.jpg')})
